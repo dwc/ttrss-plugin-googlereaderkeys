071319911e46e700ca7c4d7aee462c821a28315d
Daniel Westermann-Clark <dwc@pobox.com>
2013-07-05 00:12:03 -0700

    Sync with version from Tiny Tiny RSS 1.8

--- googlereaderkeys/init.php
+++ googlereaderkeys/init.php
@@ -1,17 +1,14 @@
 <?php
 class GoogleReaderKeys extends Plugin {
-
-	private $link;
 	private $host;
 
 	function about() {
 		return array(1.0,
-			"Map to Google Reader keys",
+			"Keyboard hotkeys emulate Google Reader",
 			"markwaters");
 	}
 
 	function init($host) {
-		$this->link = $host->get_link();
 		$this->host = $host;
 
 		$host->add_hook($host::HOOK_HOTKEY_MAP, $this);
@@ -19,16 +16,26 @@ class GoogleReaderKeys extends Plugin {
 
 	function hook_hotkey_map($hotkeys) {
 
-		$hotkeys["j"] = "next_article_noscroll";
-		$hotkeys["N"] = "next_feed";
-		$hotkeys["k"] = "prev_article_noscroll";
-		$hotkeys["P"] = "prev_feed";
-		$hotkeys["v"] = "open_in_new_window";
-		$hotkeys["(32)"] = "next_article";
-		$hotkeys["r"] = "feed_refresh";
+		$hotkeys["j"]		= "next_article_noscroll";
+		$hotkeys["k"]		= "prev_article_noscroll";
+		$hotkeys["*n"]		= "next_feed";
+		$hotkeys["*p"]		= "prev_feed";
+		$hotkeys["v"]		= "open_in_new_window";
+		$hotkeys["r"]		= "feed_refresh";
+		$hotkeys["m"]		= "toggle_unread";
+		$hotkeys["o"]		= "toggle_expand";
+		$hotkeys["(13)|enter"]	= "toggle_expand";
+		$hotkeys["*(191)|?"]    = "help_dialog";
+		$hotkeys["(32)|space"]	= "next_article";
+		$hotkeys["(38)|up"]	= "article_scroll_up";
+		$hotkeys["(40)|down"]	= "article_scroll_down";
 
 		return $hotkeys;
-
 	}
+
+	function api_version() {
+		return 2;
+	}
+
 }
 ?>
